bd92588545f01ebd891e887030eed7728dd34b81
Antonio Pereira <anthonyesany0712@gmail.com>
2023-04-01 13:16:46 -0300

    aula 04 adding sidebar with like and dislike

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Antonio"
+          description="Brecker o goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video
+          likes={444}
+          messages={555}
+          shares={656}
+          name="Pereira"
+          description="Bird olhando para a camera"
+          music="Clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
+
       </div>
     </div>
   );
